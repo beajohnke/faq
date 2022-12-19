894116091fa878aaa4a52c12702326f294f1886d
Beatriz Johnke <104087110+beajohnke@users.noreply.github.com>
2022-12-19 19:33:42 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+const iconeMais = document.querySelectorAll('.duvida');
 
+iconeMais.forEach((item) => {
+    item.addEventListener('click', () => {
+        const itemAtivo = document.querySelector('.ativo')
+
+        if(itemAtivo) {
+            itemAtivo.classList.remove('ativo')
+        }
+
+        item.classList.add('ativo')
+    })
+})
